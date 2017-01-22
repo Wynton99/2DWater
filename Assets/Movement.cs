b64f16d2fb771a0e8a7899fad7d4bee39f1875c4
wynton <kirkp1wc@cmich.edu>
2017-01-22 00:44:26 -0500

    Added Camera File Increased Angular Drag Camera Follows Ship Ship No Longer Flips Over

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,25 +2,29 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
-   // private Rigidbody2D ship;
-   // bool facingRight = false;
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D ship;
+    bool facingRight = false;
 
-	// Use this for initialization
-	void Start () {
-       // ship = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-    /*    float horizontal = Input.GetAxis("Horizontal");
-        ship.velocity = new Vector2 (horizontal * 25, ship.velocity.y);
+    //Use this for initialization
+
+   void Start () {
+        ship = GetComponent<Rigidbody2D>();
+    }
+
+    //Update is called once per frame
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        ship.velocity = new Vector2(horizontal * 30, ship.velocity.y);
         if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
         {
             facingRight = !facingRight;
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
             transform.localScale = theScale;
-        } */
-    } 
+        }
+    }
 }
